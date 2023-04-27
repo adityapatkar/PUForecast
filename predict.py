74213d48d8cb76c1a8dae03554a102d6e7e90947
Aditya Patkar <aditya.patkar@nonstopio.com>
2023-04-27 18:55:18 -0400

    Added SGD implementation

--- predict.py
+++ predict.py
@@ -7,7 +7,6 @@ def puf_query(c, w):
     phi[n] = 1
     for i in range(n-1, -1, -1):
         phi[i] = (2*c[0,i]-1)*phi[i+1]
-
     r = (np.dot(phi, w) > 0)
     return r
     
@@ -23,21 +22,33 @@ for i in range(1, n+2):
     randi_offset = np.random.randint(1, 45481)
     w[i-1] = data[randi_offset-1]
 
-# Syntax to query the PUF:
-c = np.random.randint(0, 2, size=(1, n))  # a random challenge vector
-r = puf_query(c, w)
-# you may remove these two lines
-
 # You can use the puf_query function to generate your training dataset
 # ADD YOUR DATASET GENERATION CODE HERE
-training_size = 0
 
-w0 = np.zeros((n+1, 1))  # The estimated value of w.
-# Try to estimate the value of w here. This section will be timed. You are
-# allowed to use the puf_query function here too, but it will count towards
-# the training time.
-t0 = time.process_time()
+print("Generating training set...")
+training_size = 10000
+X = np.random.randint(0, 2, size=(training_size, n)) 
+y = np.zeros((training_size, 1))
+for i in range(training_size):
+    y[i] = puf_query(X[i:i+1, :], w)
+
+print("Training set generated")
+
 # ADD YOUR TRAINING CODE HERE
+w0 = np.zeros((n+1, 1)) # The estimated value of w.
+t0 = time.process_time()
+learning_rate = 0.1
+for i in range(training_size):
+    c = X[i:i+1,:]
+    phi = np.ones(n+1)
+    phi[n] = 1
+    for j in range(n-1, -1, -1):
+        phi[j] = (2*c[0,j]-1)*phi[j+1]
+    z = np.dot(phi, w0)
+    y_pred = (z > 0)
+    error = y[i][0] - y_pred
+    delta = learning_rate * error * phi.reshape((n+1,1))
+    w0 = w0 + delta
 
 t1 = time.process_time()
 training_time = t1 - t0  # time taken to get w0
@@ -49,6 +60,7 @@ n_test = 10000
 correct = 0
 for i in range(1, n_test+1):
     c_test = np.random.randint(0, 2, size=(1, n))  # a random challenge vector
+    #convert 0 to -1
     r = puf_query(c_test, w)
     r0 = puf_query(c_test, w0)
     correct += (r==r0)
