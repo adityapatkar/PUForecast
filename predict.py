d7a2ac9485b17bd0c7c79b9ab35b0f9b57c1835e
Aditya Patkar <aditya.patkar@nonstopio.com>
2023-04-27 20:21:23 -0400

    Added logistic with sklearn

--- predict.py
+++ predict.py
@@ -1,5 +1,16 @@
 import numpy as np
 import time
+#logistic regression
+from sklearn.linear_model import LogisticRegression
+
+def transform_X(X):
+    n = X.shape[1]
+    phi_X = np.ones((X.shape[0], n+1))
+    phi_X[:, n] = 1
+    for i in range(n-1, -1, -1):
+        phi_X[:, i] = (2*X[:, i]-1)*phi_X[:, i+1]
+    return phi_X
+
 
 def puf_query(c, w):
     n = c.shape[1]
@@ -11,6 +22,7 @@ def puf_query(c, w):
     r = (np.dot(phi, w) > 0)
     return r
     
+
 # Problem Setup
 target = 0.99  # The desired prediction rate
 n = 64  # number of stages in the PUF
@@ -29,29 +41,36 @@ r = puf_query(c, w)
 # you may remove these two lines
 
 # You can use the puf_query function to generate your training dataset
-# ADD YOUR DATASET GENERATION CODE HERE
-training_size = 0
-
-w0 = np.zeros((n+1, 1))  # The estimated value of w.
-# Try to estimate the value of w here. This section will be timed. You are
-# allowed to use the puf_query function here too, but it will count towards
-# the training time.
-t0 = time.process_time()
 # ADD YOUR TRAINING CODE HERE
 
-t1 = time.process_time()
-training_time = t1 - t0  # time taken to get w0
-print("Training time:", training_time)
-print("Training size:", training_size)
+# Generate the training dataset
+print("Generating training set...")
+training_size = 4500
+X = np.random.randint(0, 2, size=(training_size, n)) 
+y = np.zeros((training_size, 1))
+for i in range(training_size):
+    y[i] = puf_query(X[i:i+1, :], w)
 
-# Evaluate your result
+print("Training set generated")
+
+# Train the decision tree
+t0 = time.process_time()
+dt = LogisticRegression(solver='lbfgs', max_iter=1000)
+X = transform_X(X)
+dt.fit(X, y.ravel())
+t1 = time.process_time()
+training_time = t1 - t0
+print("Training time:", training_time)
+
+# Evaluate the decision tree
 n_test = 10000
 correct = 0
 for i in range(1, n_test+1):
     c_test = np.random.randint(0, 2, size=(1, n))  # a random challenge vector
     r = puf_query(c_test, w)
-    r0 = puf_query(c_test, w0)
-    correct += (r==r0)
+    c_test = transform_X(c_test)
+    r_dt = dt.predict(c_test)
+    correct += (r==r_dt)
 
 success_rate = correct/n_test
 print("Success rate:", success_rate)
@@ -62,6 +81,3 @@ effective_training_time = training_time
 if success_rate < 0.99:
     effective_training_time = training_time + 10000*(0.99-success_rate)
 print("Effective training time:", effective_training_time)
-
-
-
