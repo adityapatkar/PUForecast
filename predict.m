b9c7445738f377b0536426e27ca52d57550ebf8a
Aditya Patkar <aditya.patkar@nonstopio.com>
2023-05-05 16:40:11 -0400

    Added final code for both python and matlab

--- predict.m
+++ predict.m
@@ -1,8 +1,8 @@
-% Problem Setup
+ % Problem Setup
 target = 0.99; % The desired prediction rate
-n = 8; % # of stages in the PUF
+n = 64; % # of stages in the PUF
 
-% Initialize the PUF
+% Initialize the PUF 
 rng('shuffle');
 data = load('weight_diff.txt');
 w = zeros(1+n,1);
@@ -11,48 +11,61 @@ for i = 1:1:1+n
     w(i) = data(randi_offset);
 end
 
-% Syntax to query the PUF:
-% c = randi([0 1], 1, n); % a random challenge vector
-% r = puf_query(c,w);
-% you may remove these two lines
+%testing loop
 
-% You can use the puf_query function to generate your training dataset
-% ADD YOUR DATASET GENERATION CODE HERE
-training_size = 100;
+%params
+training_size = 8000;
+repeat = 50;
+lrate = .0001;
 
-X_train = zeros(training_size, n); % initialize the input features of the training dataset
-Y_train = zeros(training_size, 1); % initialize the output labels of the training dataset
-
-for i=1:training_size
-    c_train = randi([0 1], 1, n); % generate a random challenge vector
-    X_train(i,:) = c_train; % add the challenge vector to the input features
-    Y_train(i) = puf_query(c_train, w); % calculate the corresponding response and add it to the output labels
+    
+training_set = [];
+training_res = [];
+phi_set = [];
+for i = 1:training_size
+    c = randi([0 1], 1, n);
+    r = puf_query(c,w);
+    [~,n] = size(c);
+    phi = ones(1,n+1);
+    phi(n+1) = 1;
+    for i = n:-1:1
+	    phi(i) = (2*c(i)-1)*phi(i+1);
+    end
+    training_set = [training_set; c];
+    phi_set = [phi_set; phi];
+    training_res = [training_res; r];
 end
 
-% Train an SVM
-% t0 = cputime;
-SVMModel = fitcsvm(X_train,Y_train,'ClassNames',[0,1],'Standardize',true, 'KernelFunction','RBF', 'OptimizeHyperparameters','auto'); % train an SVM model
-t1 = cputime;
-training_time = t1 - t0;
 
-% Evaluate the Model
+w0 = zeros(1+n,1); % The estimated value of w.
+t0 = cputime;
+
+%gradient descent algorithm call
+[w0, costs] = gradDesc(repeat,lrate,w0,phi_set,training_res,length(training_res));
+
+t1 = cputime;
+training_time = t1 - t0; % time taken to get w0
+
+% Evaluate your result
 n_test = 10000;
 correct = 0;
 for i=1:1:n_test
-    c_test = randi([0 1], 1, n); % generate a random challenge vector
-    r = puf_query(c_test,w); % true response
-    r0 = predict(SVMModel, c_test); % estimated response using the SVM model
-    correct  = correct + (r==r0); % count the correct predictions
+    c_test = randi([0 1], 1, n); % a random challenge vector
+    r = puf_query(c_test,w);
+    r0 = puf_query(c_test,w0);
+    correct  = correct + (r==r0);
 end
 
 success_rate = correct/n_test;
 
-% Calculate the Effective Training Time
+% If the success rate is less than 99%, a penalty time will be added
+% One second is add for each 0.01% below 99%.
 effective_training_time = training_time;
 if success_rate < 0.99
     effective_training_time = training_time + 10000*(0.99-success_rate);
 end
 
-% Display Results
-disp(['Effective Training Time: ' num2str(effective_training_time) ' seconds']);
-disp(['Success Rate: ' num2str(success_rate*100) '%']);
+disp('Success_rate');
+disp(success_rate);
+disp('Effective training time');
+disp(effective_training_time);
